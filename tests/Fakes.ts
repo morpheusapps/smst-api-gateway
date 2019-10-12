a187bba4b8bcada275572ef2de7be0f9d01a17ab
maorRoz <andory@post.bgu.ac.il>
2019-10-12 03:19:34 +0300

    updated Fakes

--- tests/Fakes.ts
+++ tests/Fakes.ts
@@ -1,8 +1,19 @@
 import faker from 'faker';
 
+function sample<T>(collection: T[]): T {
+  return collection[Math.floor(Math.random() * collection.length)];
+}
+
 export const Fakes = {
   number: (): number => faker.random.number(),
-  uuid: (): string => faker.random.uuid(),
   boolean: (): boolean => faker.random.boolean(),
-  string: (): string => faker.lorem.words().replace(/\s/g, '-')
+  string: (): string => faker.lorem.words(),
+  numberOptional: (): number | undefined =>
+    sample([faker.random.number(), undefined]),
+  booleanOptional: (): boolean | undefined =>
+    sample([faker.random.boolean(), undefined]),
+  stringOptional: (): string | undefined =>
+    sample([faker.lorem.words(), undefined]),
+  uuid: (): string => faker.random.uuid(),
+  route: (): string => `/${faker.internet.domainWord()}`
 };
