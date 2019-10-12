ecc237b9a855d8389db49f62df574a01959f7223
maorRoz <andory@post.bgu.ac.il>
2019-10-12 03:23:22 +0300

    improved Fakes logic

--- tests/Fakes.ts
+++ tests/Fakes.ts
@@ -1,19 +1,16 @@
 import faker from 'faker';
-
-function sample<T>(collection: T[]): T {
-  return collection[Math.floor(Math.random() * collection.length)];
-}
+import { Sample } from './Sample';
 
 export const Fakes = {
   number: (): number => faker.random.number(),
   boolean: (): boolean => faker.random.boolean(),
   string: (): string => faker.lorem.words(),
   numberOptional: (): number | undefined =>
-    sample([faker.random.number(), undefined]),
+    Sample([faker.random.number(), undefined]),
   booleanOptional: (): boolean | undefined =>
-    sample([faker.random.boolean(), undefined]),
+    Sample([faker.random.boolean(), undefined]),
   stringOptional: (): string | undefined =>
-    sample([faker.lorem.words(), undefined]),
+    Sample([faker.lorem.words(), undefined]),
   uuid: (): string => faker.random.uuid(),
   route: (): string => `/${faker.internet.domainWord()}`
 };
